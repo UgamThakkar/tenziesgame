bfe47f5b11e0d0ea1e6f84cced710d08dd6fe76c
Ugam Thakkar <thakkarugam@gmail.com>
2025-03-17 13:34:02 -0700

    Tenzies game first commit - initial state of the app before roll the dice and winning game features

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import Die from './components/Die';
+import { nanoid } from 'nanoid';
 function App() {
+  const [dice, setDice] = useState(generateAllNewDice());
+
+  function generateAllNewDice() {
+    return new Array(10)
+      .fill(0)
+      .map(() => ({ value: Math.ceil(Math.random() * 6), isHeld: false , id:nanoid()}));
+
+  }
+
+  function rollDice() {
+    setDice(generateAllNewDice());
+  }
+
+  function hold(id){
+    setDice(oldDice =>{
+      return oldDice.map(die =>{
+        return die.id === id?
+          {...die, isHeld:!die.isHeld}:
+          die
+
+      })
+    })
+  }
+
+  const diceElements = dice.map((die, index) => (
+    <Die key={die.id} value={die.value} isHeld={die.isHeld} hold={()=> hold(die.id)}/>
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className='dice-container'>
+        {diceElements} 
+      </div>
+
+      <button className='roll-dice' onClick={rollDice}>
+        Roll
+      </button>
+    </main>
   );
 }
 
